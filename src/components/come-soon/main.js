5f289509769ff4309e2ba54e4bebb4b6dd2b5bf5
JamilServices <jamilservicos@gmail.com>
2024-06-15 06:21:13 -0300

    :sparkles: feat(come-soon): introduce main component with SVG logo and text
    Add a new component for the 'come-soon' page featuring a complex SVG
    logo and welcome text. This includes multiple SVG groups and elements
    structured in a nested format. The main content is encapsulated in
    an article element with headers and paragraphs introducing 'Owle'
    and the upcoming 'Little Owle' products.

---
+++ src/components/come-soon/main.js
@@ -0,0 +1,193 @@
+'use strict';
+
+'use strict';
+
+const logoSvgGroup1 = {
+    type: "svg",
+    element: "g",
+    attrNS: {
+        fill: "#ec5615",
+        "fill-rule": "evenodd",
+        transform: "translate(300.934 25.955)"
+    },
+    children: [
+        {
+            type: "svg",
+            element: "path",
+            attrNS: {
+                d: "M-187.357 95.178a39.663 38.314 0 0 1-39.663 38.314 39.663 38.314 0 0 1-39.663-38.314 39.663 38.314 0 0 1 39.663-38.314 39.663 38.314 0 0 1 39.663 38.314z"
+            },
+        },
+        {
+            type: "svg",
+            element: "ellipse",
+            attrNS: {
+                cx: "-144.986",
+                cy: "72.458",
+                rx: "42.672",
+                ry: "41.221"
+            },
+        },
+    ]
+};
+const logoSvgGroup2 = {
+    type: "svg",
+    element: "g",
+    attrNS: {
+        fill: "#bd7100",
+        "fill-rule": "evenodd",
+        transform: "rotate(-5.104 460.174 282.736)"
+    },
+    children: [
+        {
+            type: "svg",
+            element: "ellipse",
+            attrNS: {
+                cx: "169.455",
+                cy: "211.673",
+                rx: "18.634",
+                ry: "12.953"
+            },
+        },
+        {
+            type: "svg",
+            element: "ellipse",
+            attrNS: {
+                cx: "121.693",
+                cy: "211.673",
+                rx: "18.634",
+                ry: "12.953"
+            },
+        },
+    ]
+};
+const logoSvgGroup3 = {
+    type: "svg",
+    element: "g",
+    attrNS: {
+        "fill-rule": "evenodd",
+        transform: "translate(300.934 25.955)"
+    },
+    children: [
+        {
+            type: "svg",
+            element: "ellipse",
+            attrNS: {
+                fill: "#fff",
+                cx: "-213.583",
+                cy: "97.259",
+                rx: "21.392",
+                ry: "19.099"
+            },
+        },
+        {
+            type: "svg",
+            element: "ellipse",
+            attrNS: {
+                fill: "#fff",
+                cx: "-157.815",
+                cy: "80.906",
+                rx: "21.392",
+                ry: "19.099"
+            },
+        },
+        {
+            type: "svg",
+            element: "ellipse",
+            attrNS: {
+                cx: "-208.035",
+                cy: "100.329",
+                rx: "12.343",
+                ry: "11.359"
+            },
+        },
+        {
+            type: "svg",
+            element: "ellipse",
+            attrNS: {
+                cx: "-157.396",
+                cy: "84.672",
+                rx: "12.343",
+                ry: "11.359"
+            },
+        },
+    ]
+};
+
+const logoSvg = {
+    type: "svg",
+    element: "svg",
+    attr: {
+        class: "logo-svg",
+    },
+    attrNS: {
+        viewBox: "0 0 248.496 255.073"
+    },
+    children: [
+        {
+            type: "svg",
+            element: "circle",
+            attr: {
+                class: "owle-background"
+            },
+            attrNS: {
+                cx: "-176.686",
+                cy: "98.293",
+                r: "124.248",
+                "fill-rule": "evenodd",
+                transform: "translate(300.934 25.955)"
+            },
+        },
+        {
+            type: "svg",
+            element: "path",
+            attr: {
+                class: "owle-body"
+            },
+            attrNS: {
+                //fill: "#22241c",
+                "fill-rule": "evenodd",
+                d: "M31.568 88.216a38.32 38.32 0 0 1-.42-5.097c.059-2.644.023-7.203 1.243-10.315.532-1.358 1.302-2.44 2.424-2.968.644-.345 1.433-.508 2.297-.58 1.378-.113 2.946-.038 4.41.002l9.517.261c3.251.099 6.51.224 9.785.37 4.596.207 9.23.381 13.924.657l10.28.392c1.474.057 2.872-.73 4.296-1.244a33.903 33.903 0 0 0 3.577-1.528c1.82-.78 3.453-1.468 5.038-2.1a93.198 93.198 0 0 1 5.14-1.89c1.282-.428 2.652-.778 4.143-1.156 1.416-.403 2.548-.66 3.745-1.209 1.707-.783 3.085-1.864 4.809-3.182 6.1-5.268 14.115-11.82 21.059-17.298.818-.646 1.66-1.225 2.413-1.87 2.24-1.923 4.405-3.508 6.243-4.857 1.078-.791 2.857-2.111 3.634-2.63 1.072-.692 1.78-1.14 2.343-1.335 1.225-.427 2.837.255 4.171 1.243 1.344 1.125 2.233 2.178 2.873 3.123.697 1.028.87 1.18 1.24 1.908l37.005 102.648c.855 4.346 2.048 10.638 2.768 16.642.95 7.903 1.081 15.307.503 22.203-1.023 12.206-4.27 22.819-9.145 31.79-12.411 22.838-35.371 35.032-59.042 35.776-17.96.564-36.33-5.465-50.81-18.44-8.852-7.931-16.25-18.46-21.211-31.663-2.674-7.116-4.64-15.01-5.746-23.693-.68-5.35-1.034-10.998-1.03-15.031z"
+            },
+        },
+        logoSvgGroup1,
+        {
+            type: "svg",
+            element: "path",
+            attrNS: {
+                fill: "#ff9d43",
+                d: "m132.217 169.112.7-3.895c.893-4.956 2.3-17.756 2.26-20.58-.048-3.6-.476-3.97-5.518-4.774-6.7-1.068-7.704-.976-9.77.895-3.9 3.533-6.224 6.184-6.212 7.085.012.887.329 1.439 3.077 5.367 1.98 2.83 14.281 16.795 14.82 16.824.26.013.55-.4.643-.922z"
+            },
+        },
+        logoSvgGroup2,
+        logoSvgGroup3
+    ]
+};
+
+const mainText = {
+    element: "article",
+    children: [
+        {
+            element: "h4",
+            text: "Ol\u00E1!"
+        },
+        {
+            element: "p",
+            text: "Eu sou Owle, e represento a nova linha de produtos 'JamilServices'."
+        },
+        {
+            element: "p",
+            text: "Em breve, eu irei lhe apresentar os produtos 'Little Owle'."
+        },
+    ]
+}
+
+const helloWordExample = {
+    element: "main",
+    children: [
+        logoSvg,
+        mainText
+    ]
+};
+
+export const comeSoonPage = Object.freeze(helloWordExample);
